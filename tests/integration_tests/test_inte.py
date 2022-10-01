ea0927d6b2a65f47b941f402ff8d0b85240edba4
NotAndex <85022950+NotAndex@users.noreply.github.com>
2022-10-01 17:04:29 +0200

    asda

--- tests/integration_tests/test_inte.py
+++ tests/integration_tests/test_inte.py
@@ -1,6 +1,11 @@
 import pytest
 
 from transformation_2.step_1 import subtraction
+from transformation_1.step_1 import addition
+
 
 def test_subtraction():
     assert subtraction(2,2) is 1
+
+def test_addition():
+    assert addition(2,2) is 3
