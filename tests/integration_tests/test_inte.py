a63ac2ca53931188bbf88e92d1e58fe508f5826b
NotAndex <85022950+NotAndex@users.noreply.github.com>
2022-10-01 16:46:13 +0200

    asdadsasd

--- tests/integration_tests/test_inte.py
+++ tests/integration_tests/test_inte.py
@@ -3,5 +3,4 @@ import pytest
 from transformation_2.step_1 import subtraction
 
 def test_subtraction():
-    assert subtraction(2,2) is 0
-    assert subtraction(4,2) is 1
+    assert subtraction(2,2) is 1
